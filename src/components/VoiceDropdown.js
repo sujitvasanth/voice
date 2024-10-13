ff1cd813b1a7d2531b4d0bd881900f0bec0b0fa0
Dr Sujit Vasanth <sujitvasanth@gmail.com>
2024-10-13 23:35:23 +0100

    Update VoiceDropdown.js

--- src/components/VoiceDropdown.js
+++ src/components/VoiceDropdown.js
@@ -1,20 +1,15 @@
 import React from 'react';
 
-function VoiceDropdown({ voices = [], onVoiceChange }) {
+function VoiceDropdown({ voices, onVoiceChange }) {
   return (
     <select onChange={(e) => onVoiceChange(e.target.value)}>
-      {voices.length > 0 ? (
-        voices.map((voice) => (
-          <option key={voice.id} value={voice.id}>
-            {voice.name}
-          </option>
-        ))
-      ) : (
-        <option>No voices available</option>
-      )}
+      {voices.map((voice) => (
+        <option key={voice.id} value={voice.id}>
+          {voice.name}
+        </option>
+      ))}
     </select>
   );
 }
 
 export default VoiceDropdown;
-
