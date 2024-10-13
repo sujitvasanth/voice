edcabc55fce89b24ee0a89ebbb052fe6c00b1d7c
Dr Sujit Vasanth <sujitvasanth@gmail.com>
2024-10-13 20:55:17 +0100

    Update _app.tsx
    adding dropdown test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,17 @@ import { CloudProvider } from "@/cloud/useCloud";
 import "@livekit/components-styles/components/participant";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import VoiceDropdown from "../components/VoiceDropdown"; // Import the dropdown
+
+function handleVoiceChange(voiceId: string) {
+  // Implement your logic here (publish data to LiveKit or console log for testing)
+  console.log("Selected voice:", voiceId);
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CloudProvider>
+      <VoiceDropdown onVoiceChange={handleVoiceChange} />
       <Component {...pageProps} />
     </CloudProvider>
   );
