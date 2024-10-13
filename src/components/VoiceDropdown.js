ab5d5698a9c16b3d134eebe27a0cd8bbf7f042d1
Dr Sujit Vasanth <sujitvasanth@gmail.com>
2024-10-13 22:09:00 +0100

    Update VoiceDropdown.js
    corrected build error

--- src/components/VoiceDropdown.js
+++ src/components/VoiceDropdown.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
-function VoiceDropdown({ voices, onVoiceChange }) {
+function VoiceDropdown({ voices = [], onVoiceChange }) {
   return (
     <select onChange={(e) => onVoiceChange(e.target.value)}>
-      {voices.map((voice) => (
-        <option key={voice.id} value={voice.id}>
-          {voice.name}
-        </option>
-      ))}
+      {voices.length > 0 ? (
+        voices.map((voice) => (
+          <option key={voice.id} value={voice.id}>
+            {voice.name}
+          </option>
+        ))
+      ) : (
+        <option>No voices available</option>
+      )}
     </select>
   );
 }
