8fd66afe85774ddf3eb5c1a6bc738ff234cbc91d
Dr Sujit Vasanth <sujitvasanth@gmail.com>
2026-04-26 20:36:27 +0100

    Update _document.tsx
    update to html chat window

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css"/>
+        <script defer src="https://cdn.jsdelivr.net/npm/marked@9.1.6/marked.min.js"/>
+        <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.js"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
